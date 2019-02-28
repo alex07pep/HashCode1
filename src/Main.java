c29c80e86a3f3000a7e6c68ab95408ac2a84dfd6
marius2010t <marius2010t@yahoo.com>
2019-02-28 22:03:33 +0200

    Default Changelist

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-       ArrayList<Photo> list1 = Reader.read("resources/e_shiny_selfies.txt");
+       ArrayList<Photo> list1 = Reader.read("resources/b_lovely_landscapes.txt");
 
        ArrayList<Photo> verticalPhotos = new ArrayList<Photo>();
        ArrayList<Photo> horizontalPhotos = new ArrayList<Photo>();
@@ -26,14 +26,46 @@ public class Main {
         finalSlides.addAll(horizontalPhotos);
         finalSlides.addAll(verticalSlides);
 
-        FileWriter fileWriter = new FileWriter("resources/out_e_shiny_selfies.txt");
+        ArrayList<Photo> forPrintSlides = new ArrayList<Photo>();
+
+        int n = 0;
+        int max = -1;
+        int index=-1;
+        Photo photo = finalSlides.get(0);
+        finalSlides.remove(0);
+        forPrintSlides.add(photo);
+
+        while(finalSlides.size()>0) {
+
+            n = 0;
+            max = -1;
+            index = -1;
+            n = finalSlides.size();
+            System.out.println(n);
+            for (int i = 0; i < n; i++) {
+                int micut = MinClass.Min(photo, finalSlides.get(i));
+                if (micut > max) {
+                    max = micut;
+                    index = i;
+                }
+                if(i>100)
+                    break;
+            }
+            photo=finalSlides.get(index);
+            forPrintSlides.add(photo);
+            finalSlides.remove(index);
+        }
+
+
+
+
+        FileWriter fileWriter = new FileWriter("resources/1out_b_lovely_landscapes.txt.txt");
         PrintWriter printWriter = new PrintWriter(fileWriter);
-        printWriter.println(finalSlides.size()+"");
-        for(Photo p : finalSlides){
+        printWriter.println(forPrintSlides.size()+"");
+        for(Photo p : forPrintSlides){
             printWriter.println(p.id);
         }
         printWriter.close();
-        System.out.println(MinClass.Min(finalSlides.get(0),finalSlides.get(1)));
-        System.out.println(MinClass.Min(finalSlides.get(2),finalSlides.get(1)));
+
     }
 }
